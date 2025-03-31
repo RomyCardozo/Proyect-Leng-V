85ba46cf2890c12ea18e415cda3212b74120fe1a
RomyCardozo <romycardozopy@gmail.com>
2025-03-30 22:43:15 -0300

    Actualizar estilos y funcionalidad de la página de inicio de sesión, agregar soporte para Firebase y mejorar la gestión de menús

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { environment } from './../environments/environment';
 
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -11,16 +10,27 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule
-
-  ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideFirebaseApp(() => initializeApp( environment.firebaseConfig )),
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
-
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'romina-proyect',
+        appId: '1:511869476616:web:056cdc48fc07aaaf8f96bd',
+        storageBucket: 'romina-proyect.firebasestorage.app',
+        apiKey: 'AIzaSyDN7QagcoBS4821DrlpvO0khMNJ2qFuabE',
+        authDomain: 'romina-proyect.firebaseapp.com',
+        messagingSenderId: '511869476616',
+        measurementId: 'G-425NMC1YS0',
+      })
+    ),
+    provideAuth(() => getAuth()),
   ],
   bootstrap: [AppComponent],
 })
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuCtrl: MenuController) { }
 
   ngOnInit() {
+    this.menuCtrl.enable(false); // Deshabilitar el menú en la página de inicio de sesión
+
+  }
+
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false); // Deshabilitar el menú al entrar a la página de inicio de sesión
+  }
+  ionViewWillLeave() {
+    this.menuCtrl.enable(true); // Habilitar el menú al salir de la página de inicio de sesión
   }
 
 }
